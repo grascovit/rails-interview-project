552701b4137b65048158760e70e3ff0cdc5f67f0
Aaron Pearce <me@aaronpearce.com>
2017-08-18 12:11:43 +1200

    Update our test project to run on Rails 5.1

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,30 +13,30 @@
 ActiveRecord::Schema.define(version: 20160512220935) do
 
   create_table "answers", force: :cascade do |t|
-    t.string   "body",        null: false
-    t.integer  "question_id", null: false
-    t.integer  "user_id",     null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "body", null: false
+    t.integer "question_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title",                      null: false
-    t.boolean  "private",    default: false
-    t.integer  "user_id",                    null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string "title", null: false
+    t.boolean "private", default: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tenants", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "api_key",    null: false
+    t.string "name", null: false
+    t.string "api_key", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
