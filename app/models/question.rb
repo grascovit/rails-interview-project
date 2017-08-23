96542cd228112e753426afe14d36438f312a3485
Gabriel Rascovit <rascovit@gmail.com>
2017-08-23 18:51:59 -0300

    create question scope to return only public questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers
   belongs_to :user
+
+  scope :public_only, -> { where(private: false) }
 end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:answers) }
+  end
+
+  describe 'scopes' do
+    describe '.public_only' do
+      context 'when there are public and private questions' do
+        let(:public_question) { create(:public_question) }
+        let(:private_question) { create(:private_question) }
+
+        it 'returns only the public questions' do
+          another_public_question = create(:public_question)
+
+          expect(Question.public_only).to include(public_question, another_public_question)
+        end
+
+        it 'does not include the private question' do
+          expect(Question.public_only).not_to include(private_question)
+        end
+      end
+
+      context 'when there are no questions' do
+        it 'returns empty array' do
+          expect(Question.public_only).to eq([])
+        end
+      end
+    end
+  end
+end
