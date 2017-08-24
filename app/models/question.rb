090aa1ae1533fe34019e9c11d4a9d966110dc2a2
Gabriel Rascovit <rascovit@gmail.com>
2017-08-24 17:00:30 -0300

    create scope and class method to search questions by title

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,15 @@ class Question < ApplicationRecord
   belongs_to :user
 
   scope :public_only, -> { where(private: false) }
+  scope :search_by_title, ->(query) {
+    where('LOWER(title) LIKE :query', query: "%#{query.to_s.downcase}%")
+  }
+
+  def self.search(query)
+    if query.present?
+      search_by_title(query)
+    else
+      all
+    end
+  end
 end
