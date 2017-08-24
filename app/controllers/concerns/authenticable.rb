f3ef200abdc68df2dc8314378b8c0e4e3f83b612
Gabriel Rascovit <rascovit@gmail.com>
2017-08-23 23:16:09 -0300

    create authenticable concern

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,28 @@
+module Authenticable
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  def authenticate_tenant
+    authenticate_key || render_unauthorized
+  end
+
+  private
+
+  def authenticate_key
+    authenticate_with_http_token do |api_key|
+      tenant = Tenant.find_by(api_key: api_key)
+
+      if tenant
+        ActiveSupport::SecurityUtils.secure_compare(
+            Digest::SHA256.hexdigest(api_key),
+            Digest::SHA256.hexdigest(tenant.api_key)
+        )
+
+        return tenant
+      end
+    end
+  end
+
+  def render_unauthorized
+    head :unauthorized
+  end
+end
